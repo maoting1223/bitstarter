f11dfe0a59cd5ede384e945cad3b70fb9992a546
Ting Mao <maoting@alum.mit.edu>
2014-07-19 04:18:00 +0000

    change again

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Welcome to Ting Mao Startup Engineering Website')
+  response.send('Hello World from index.html')
 })
 
 app.listen(app.get('port'), function() {
