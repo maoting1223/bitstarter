8ad6583a04856df17b98f18bf18f0593f994ce6c
Ting Mao <maoting@alum.mit.edu>
2014-07-20 04:15:00 +0000

    rendering html

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var express = require('express')
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
 
-app.get('/', function(request, response) {
-  response.send('Hello World from index.html')
-})
+app.get('/about', function (req, res)
+{
+    res.render('index.html');
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
