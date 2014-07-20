719c8e5ce612ac15cb98f223f030b3a5ea09630c
Ting Mao <sirenhally@gmail.com>
2014-07-20 16:42:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var data = fs.readFileSync("index.html", "utf-8");
 
 var app = express.createServer(express.logger());
 
-app.set('port', (process.env.PORT || 5000));
-
 app.get('/', function(request, response) {
   response.send(data);
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+var port = process.env.PORT || 5000; 
+
+app.listen(port, function() {
+  console.log("Node app is running at localhost:" + port);
 });
