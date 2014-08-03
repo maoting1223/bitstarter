10ce0752d65f98baa56ff56fa4758232a6ad80b1
Ting Mao <maoting@alum.mit.edu>
2014-08-03 05:09:10 +0000

    update index.js

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ var data = fs.readFileSync('index.html', 'utf-8');
 <<<<<<< HEAD
 var app = express();
 var server = http.createServer(app);
-=======
-var app = express.createServer(express.logger());
->>>>>>> 719c8e5ce612ac15cb98f223f030b3a5ea09630c
 
 app.get('/', function(request, response) {
   response.send(data);
@@ -16,9 +13,6 @@ app.get('/', function(request, response) {
 
 <<<<<<< HEAD
 var port = process.env.PORT || 5000;
-=======
-var port = process.env.PORT || 5000; 
->>>>>>> 719c8e5ce612ac15cb98f223f030b3a5ea09630c
 
 app.listen(port, function() {
   console.log("Node app is running at localhost:" + port);
