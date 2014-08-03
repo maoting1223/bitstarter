e899715ea33ad7b4a94da245b3ef9adeed820f36
Ubuntu <ubuntu@ip-172-31-20-159.us-west-2.compute.internal>
2014-08-03 05:32:43 +0000

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 
 var data = fs.readFileSync('index.html', 'utf-8');
 
-<<<<<<< HEAD
 var app = express();
 var server = http.createServer(app);
 
@@ -11,8 +10,7 @@ app.get('/', function(request, response) {
   response.send(data);
 });
 
-<<<<<<< HEAD
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 
 app.listen(port, function() {
   console.log("Node app is running at localhost:" + port);
