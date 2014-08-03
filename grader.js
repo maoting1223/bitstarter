c4e68bdc395a523f0f3c4edddf4174936a51f2d5
Ting Mao <maoting@alum.mit.edu>
2014-08-03 04:30:37 +0000

    updated checks, grader, index.html

--- grader.js
+++ grader.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+http://mighty-oasis-7430.herokuapp.com/
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -23,52 +23,49 @@ References:
 
 var fs = require('fs');
 var program = require('commander');
-var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var rest = require('restler');
+var URL_DEFAULT = "http://mighty-oasis-7430.herokuapp.com/";
 var CHECKSFILE_DEFAULT = "checks.json";
+var cheerio = require('cheerio');
 
-var assertFileExists = function(infile) {
-    var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
-    return instr;
-};
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
+var assertFileExists = function(url, fn) {
+    rest.get(url).on("complete", function(result){
+	var instr = result.toString();
+	fn(instr);
+    });
+}
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
+var checkHtmlFile = function(url, checksfile, fn) {
+    assertFileExists(url, function(instr){
+	$ = cheerio.load(instr);
+	var checks = loadChecks(checksfile).sort();
+	console.log("checks", checks)
+	var out = {};
+	for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
-    }
-    return out;
+        }
+        fn(out);
+    })
 };
 
-var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
-    return fn.bind({});
-};
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json')
+        .option('-f, --file <url>', 'Path to url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        checkHtmlFile(program.file, program.checks, function(checkJson){
+        var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    })
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
+console.log(program.file)
